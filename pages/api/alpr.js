1cbe4ffa73b42359a365869474e80d4dfba77756
Henok <henokmicrometre@gmail.com>
2023-11-30 01:52:47 +0000

    added docker-mysql/

--- pages/api/alpr.js
+++ pages/api/alpr.js
@@ -54,6 +54,8 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache, no-transform");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     res.setHeader("Connection", "keep-alive");
     let counter = 0;
     stream.on("channel", function (event, data) {
