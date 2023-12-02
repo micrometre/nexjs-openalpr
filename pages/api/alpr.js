eb3b6136a64d6be9e6d161045955d59b7e68d86c
Henok Wehibe <henokmicrometre@gmail.com>
2023-12-02 12:22:50 +0000

    added mysql

--- pages/api/alpr.js
+++ pages/api/alpr.js
@@ -1,6 +1,52 @@
 var mysql = require('mysql');
 import { watch } from 'fs';
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "alpruser",
+  password: "=[-p0o9i8U",
+  database: "alprdata"
+
+});
+
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sqlimages = `CREATE TABLE IF NOT EXISTS alpr_images (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    img TEXT,
+    created_on DATETIME NOT NULL DEFAULT NOW() -- or CURRENT_TIMESTAMP
+  )`
+  var sqlplates = `CREATE TABLE IF NOT EXISTS alpr_plates (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    plate TEXT,
+    uuid TEXT,
+    created_on DATETIME NOT NULL DEFAULT NOW() -- or CURRENT_TIMESTAMP
+  )`
+  con.query(sqlimages, function (err, result) {
+    if (err) throw err;
+    console.log("Tables created");
+  });
+  con.query(sqlplates, function (err, result) {
+    if (err) throw err;
+    console.log("Tables created");
+  });
+});
+
+
+watch('./public/images', (eventType, filename) => {
+  const sqlValues = ["http://localhost:3000/images/" + filename]
+  let sql = `INSERT INTO alpr_images(img) VALUES(?)`;
+  console.log(sqlValues)
+  con.query(sql, sqlValues, function (err, result) {
+    if (err) throw err;
+    console.error();
+  });
+});
+
 
 
 export default async function handler(req, res) {
--- pages/api/images.js
+++ pages/api/images.js
@@ -1,46 +1,6 @@
 import { watch } from 'fs';
 import EventEmitter from "events";
 
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "alpruser",
-  password: "=[-p0o9i8U",
-  database: "alprdata"
-
-});
-
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  //var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  var sql =  `CREATE TABLE IF NOT EXISTS alpr_images (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    img TEXT,
-    created_on DATETIME NOT NULL DEFAULT NOW() -- or CURRENT_TIMESTAMP
-  )`
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
-});
-
-
-const datetime = new Date();
-
-watch('./public/images', (eventType, filename) => {
-  const sqlValues = [ "http://localhost:3000/images/" + filename]
-  let sql = `INSERT INTO alpr_images(img) VALUES(?)`;
-    console.log(sqlValues)
-  con.query(sql, sqlValues, function (err, result) {
-    if (err) throw err;
-    console.error();
-    //res.json({ result })
-  });
-  });
-
 
 export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const stream = new EventEmitter();
