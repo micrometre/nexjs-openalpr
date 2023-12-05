9b1cb76b04c5dbb3186a0115a571c2cf56418cd2
Henok <henokmicrometre@gmail.com>
2023-12-05 08:55:12 +0000

    woking on

--- manage.sh
+++ manage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xv
 pwd
-alpr -c gb  -n 1 public/images | sed 's|public|{img: "http://localhost|g;s|plate0|{|g;s/.$/"}/;/: 1 result/d;s/^\s*./{/g; s/confidence:/:"/g   '
 
+alpr -c gb  -n 1 public/images | sed 's|public|"http://localhost|g;s|plate0|{|g;s/.$/"]/;/: 1 result/d;s/^\s*./["/g; s/confidence:/","/g   '
 
 
 
